7bf715915b1eb216c82eba00858e46ab92a824cb
davide-lr <davide.larosa@isti.cnr.it>
2015-08-03 18:14:42 +0200

    Refactored with background sensors sampling service

--- wear/src/main/java/it/cnr/isti/doremi/sleeplogger/MyActivity.java
+++ wear/src/main/java/it/cnr/isti/doremi/sleeplogger/MyActivity.java
@@ -17,301 +17,128 @@
 package it.cnr.isti.doremi.sleeplogger;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.PowerManager;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.CountDownLatch;
-
-public class MyActivity extends Activity /*implements SensorEventListener*/
+public class MyActivity extends Activity
 {
-    private static final String TAG = MyActivity.class.getName();
-    private static final int SAMPLING_INTERVAL = 100;                 // sampling interval [ms]
+	private static final String TAG = MyActivity.class.getName();
 
-    private TextView rate = null;
-    private TextView accuracy = null;
-    private TextView sensorInformation = null;
-    private TextView acceleration = null;
-    private static final int SENSOR_TYPE_HEARTRATE = 65562;
-    private Sensor mHeartRateSensor;
-    private Sensor mAccelerationSensor;
-    private Sensor mGyroscopeSensor;
-    private Sensor mOrientationSensor;
-    private Sensor mStepSensor;
-    private SensorManager mSensorManager;
-    private CountDownLatch latch;
-    private FileOutputStream fos = null;
-    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS", Locale.ENGLISH);
-    private PendingIntent pintent;
-    private AlarmManager alarm;
-    private static final int SAMPLING_FREQ = 1000 / SAMPLING_INTERVAL;   // sampling freq [Hz]
-    private volatile float lastHB = 0;
-    private volatile float lastStepCount = 0;
-    private volatile float[] lastAcc = new float[] {0, 0, 0};
-    private volatile float[] lastGyro = new float[] {0, 0, 0};
-    private volatile float[] lastOrientation = new float[] {0, 0, 0};
-    private PowerManager pm;	// Required to keep the service active
-    private PowerManager.WakeLock wl;
-    private int samples = 0;
-    private Thread tLogger = null;
-    private SensorEventListener selHB = null;
-    private SensorEventListener selAcc = null;
-	private SensorEventListener selOrientation = null;
-    private SensorEventListener selStep = null;
+	private TextView rate = null;
+	private TextView sensorInformation = null;
+	private Button btnStart = null;
 
-	private static final String LOG_HEADER = "TS,  BPM, AccX, AccY, AccZ\r\n";
-	private static final String LOG_FORMAT = "%d,%3.1f,%3.3f,%3.3f,%3.3f\r\n";
-
-    private class MyBroadcastReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive (Context context, Intent intent)
-        {
-            if (fos != null)
-            {
-                try {
-                    fos.write(String.format(Locale.ENGLISH, LOG_FORMAT,
-                            System.currentTimeMillis(),
-                            lastHB,
-                            lastAcc[0], lastAcc[1], lastAcc[2]
-                            /*lastOrientation[0], lastOrientation[1], lastOrientation[2]*/).getBytes());
-                    //Log.d(TAG, "Sensor data written to file: " + lastOrientation[0] + ", " + lastOrientation[1] + ", " + lastOrientation[2]);
-
-                    samples++;
-                    sensorInformation.setText ("Samples # (" + SAMPLING_FREQ + " Hz): " + samples);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    };
-
-    MyBroadcastReceiver br = new MyBroadcastReceiver ();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d (TAG, "onCreate()");
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-        latch = new CountDownLatch(1);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                rate = (TextView) stub.findViewById(R.id.rate);
-                rate.setText("Reading...");
-
-                //accuracy = (TextView) stub.findViewById(R.id.accuracy);
-                sensorInformation = (TextView) stub.findViewById(R.id.sensor);
-                //acceleration = (TextView) stub.findViewById(R.id.acceleration);
-
-                latch.countDown();
-            }
-        });
-
-        mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
-
-        for (Sensor s : mSensorManager.getSensorList(Sensor.TYPE_ALL))
-        {
-            Log.d(TAG, "Sensor: " + s.getName() + ", type = " + s.getType());
-        }
-
-        mHeartRateSensor = mSensorManager.getDefaultSensor(SENSOR_TYPE_HEARTRATE); // using Sensor Lib2 (Samsung Gear Live)
-        //mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-
-        mAccelerationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mGyroscopeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        mStepSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
-        /*try {
-            latch.await();
-        } catch (InterruptedException e)
-        {
-            System.exit (-1);
-        }*/
-
-        selHB = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                lastHB = sensorEvent.values[0];
-                /*if (rate != null)
-                    rate.setText (String.valueOf (lastHB));
-                if (accuracy != null)
-                    accuracy.setText("Accuracy: " + sensorEvent.accuracy);*/
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
-        mSensorManager.registerListener(selHB, this.mHeartRateSensor, 1000000);
-
-        selAcc = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                lastAcc = sensorEvent.values;
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
-        mSensorManager.registerListener(selAcc, this.mAccelerationSensor, SAMPLING_INTERVAL * 1000);
-
-        /*mSensorManager.registerListener(new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                lastGyro = sensorEvent.values;
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        }, this.mGyroscopeSensor, SAMPLING_INTERVAL * 1000);
-		*/
-
-		/*selOrientation = new SensorEventListener() {
-			@Override
-			public void onSensorChanged(SensorEvent sensorEvent) { lastOrientation = sensorEvent.values; }
-
-			@Override
-			public void onAccuracyChanged(Sensor sensor, int i) {
-
+	private Runnable m_UIUpdater = new Runnable()
+	{
+		@Override
+		public void run()
+		{
+			Log.d(TAG, "Updating UI..");
+			if (sensorInformation != null)
+			{
+				sensorInformation.setText("Samples: " + SensorSamplingService.getSamples());
+				sensorInformation.postDelayed(m_UIUpdater, 1000);
 			}
-		};
-        mSensorManager.registerListener(selOrientation, this.mOrientationSensor, SAMPLING_INTERVAL * 1000);
-*/
-		/*
-        selStep = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) { lastStepCount = sensorEvent.values[0]; }
+		}
+	};
 
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		Log.d (TAG, "onCreate()");
+		super.onCreate(savedInstanceState);
 
-            }
-        };
-        mSensorManager.registerListener(selStep, this.mStepSensor, 1000000);*/
+		setContentView(R.layout.activity_my);
 
-        File logFile = new File(Environment.getExternalStorageDirectory() + File.separator + "logs" + File.separator + "hb_log-" + sdf.format(new Date()) + ".txt");
-        try {
-            logFile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+		stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+			@Override
+			public void onLayoutInflated(WatchViewStub stub) {
 
-        try {
-            Log.d (TAG, "Log saved to : " + logFile.getAbsolutePath());
-            fos = new FileOutputStream(logFile);
+				rate = (TextView) stub.findViewById(R.id.rate);
+				sensorInformation = (TextView) stub.findViewById(R.id.sensor);
 
-			fos.write(String.format(Locale.ENGLISH, LOG_HEADER).getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+				btnStart = (Button) stub.findViewById(R.id.btnStart);
+				btnStart.setOnClickListener(new View.OnClickListener()
+				{
+					@Override
+					public void onClick(View view) {
+						if (!SensorSamplingService.isStarted())
+						{
+							Intent myIntent = new Intent(MyActivity.this, SensorSamplingService.class);
+							startService(myIntent);
 
-        getApplicationContext ().registerReceiver (br, new IntentFilter("it.cnr.isti.giraff.android.wear.WAKEUP"));
+							btnStart.setText("STOP");
+							btnStart.setTextColor(Color.RED);
+							rate.setText("Sampling..");
 
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.SECOND, 1);
+							sensorInformation.post(m_UIUpdater);
+						}
+						else
+						{
+							Intent myIntent = new Intent(MyActivity.this, SensorSamplingService.class);
+							stopService(myIntent);
 
-        Intent i = new Intent("it.cnr.isti.giraff.android.wear.WAKEUP");
-        pintent = PendingIntent.getBroadcast(this.getApplicationContext(), 14, i, 0);
+							btnStart.setText("START");
+							btnStart.setTextColor(Color.GREEN);
+							rate.setText("Ready");
 
-        alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), SAMPLING_INTERVAL, pintent);
-        Log.d (TAG, "Periodic wakeup set");
+							sensorInformation.removeCallbacks(m_UIUpdater);
+						}
+					}
+				});
 
-        /*pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        if (wl == null)
-            wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Logger-Lock");
-        if (!wl.isHeld ())
-            wl.acquire();*/
-    }
-
-    @Override
-    protected void onStart() {
-
-		Log.d(TAG, "onStart()");
-		super.onStart();
-
+				if (SensorSamplingService.isStarted())
+				{
+					btnStart.setText("STOP");
+					btnStart.setTextColor(Color.RED);
+					rate.setText("Sampling..");
+					sensorInformation.post(m_UIUpdater);
+				}
+				else
+				{
+					btnStart.setText("START");
+					btnStart.setTextColor(Color.GREEN);
+					rate.setText("Ready");
+					sensorInformation.setText("Samples: ---");
+				}
+			}
+		});
 	}
 
-   @Override
-    protected void onStop() {
-        Log.d (TAG, "onStop()");
+	@Override
+	protected void onStart() {
+		Log.d(TAG, "onStart()");
+		super.onStart();
+	}
 
-        super.onStop();
+	@Override
+	protected void onStop()	{
+		Log.d (TAG, "onStop()");
+		super.onStop();
+	}
 
-    }
+	@Override
+	protected void onDestroy() {
+		Log.d (TAG, "onDestroy()");
+		super.onDestroy();
+	}
 
-    @Override
-    protected void onDestroy() {
-        Log.d (TAG, "onDestroy()");
-        super.onDestroy();
+	@Override
+	protected void onPause() {
+		Log.d (TAG, "onPause()");
+		super.onPause();
+	}
 
-        if (fos != null) {
-            try {
-                fos.close();
-                Log.d (TAG, "File closed");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if (selHB != null)
-            mSensorManager.unregisterListener(selHB);
-        if (selAcc != null)
-            mSensorManager.unregisterListener(selAcc);
-		if (selOrientation != null)
-			mSensorManager.unregisterListener(selOrientation);
-        if (selStep != null)
-            mSensorManager.unregisterListener(selStep);
-
-        alarm.cancel (pintent);
-        getApplicationContext ().unregisterReceiver (br);
-
-        /*if (wl != null)
-        {
-            wl.release();
-            wl = null;
-        }*/
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d (TAG, "onPause()");
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d (TAG, "onResume()");
-        super.onResume();
-    }
+	@Override
+	protected void onResume() {
+		Log.d (TAG, "onResume()");
+		super.onResume();
+	}
 }
---
+++ wear/src/main/java/it/cnr/isti/doremi/sleeplogger/SensorSamplingService.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (C) 2014-2015 ISTI - CNR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.cnr.isti.doremi.sleeplogger;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Environment;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class SensorSamplingService extends Service
+{
+	private static final String TAG = "SensorSamplingService";
+	private static final int SAMPLING_INTERVAL = 100;                  // sampling interval [ms]
+	private static final int SAMPLING_FREQ = 1000 / SAMPLING_INTERVAL; // sampling freq [Hz]
+	private static final int SENSOR_TYPE_HEARTRATE_GEAR_LIVE = 65562;  // Samsung Gear Live custom HB sensor
+
+	private volatile float lastHB = 0;
+	private volatile float[] lastAcc = new float[] {0, 0, 0};
+
+	private static int samples = 0;
+	private SensorManager mSensorManager;
+	private Sensor mHeartRateSensor;
+	private Sensor mAccelerationSensor;
+	private SensorEventListener selHB = null;
+	private SensorEventListener selAcc = null;
+	private PowerManager powerManager;
+	private PowerManager.WakeLock wakeLock;
+	private BufferedWriter bw = null;
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS", Locale.ENGLISH);
+
+	// ADDITIONAL SENSORS, ENABLE IF NEEDED (!) modify the header and format strings accordingly
+/*	private Sensor mGyroscopeSensor;
+	private Sensor mOrientationSensor;
+	private Sensor mStepSensor;
+	private volatile float[] lastGyro = new float[] {0, 0, 0};
+	private volatile float[] lastOrientation = new float[] {0, 0, 0};
+	private volatile float lastStepCount = 0;
+	private SensorEventListener selGyroscope = null;
+	private SensorEventListener selOrientation = null;
+	private SensorEventListener selStep = null;
+*/
+	private static final String LOG_HEADER = "TS,  BPM, AccX, AccY, AccZ\r\n";
+	private static final String LOG_FORMAT = "%d,%3.1f,%3.3f,%3.3f,%3.3f\r\n";
+
+	private static volatile boolean isRunning = false;
+	private static volatile boolean isStarted = false;
+	private Thread t = new Thread(new Runnable()
+	{
+		@Override
+		public void run()
+		{
+			while (isRunning)
+			{
+				if (bw != null)
+				{
+					try {
+						bw.write(String.format(Locale.ENGLISH, LOG_FORMAT,
+								System.currentTimeMillis(),
+								lastHB,
+								lastAcc[0], lastAcc[1], lastAcc[2]));
+
+						samples++;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {}
+			}
+		}
+	});
+
+	public SensorSamplingService()
+	{
+	}
+
+	public static boolean isStarted ()
+	{
+		return isStarted;
+	}
+
+	public static int getSamples ()
+	{
+		return samples;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		return null; // service is unbounded so no need to bind
+	}
+
+	@Override
+	public void onCreate ()
+	{
+		super.onCreate();
+		Log.i(TAG, "onCreate()");
+
+		mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+
+		/*for (Sensor s : mSensorManager.getSensorList(Sensor.TYPE_ALL)) // print all the available sensors
+		{
+			Log.d(TAG, "Sensor: " + s.getName() + ", type = " + s.getType());
+		}*/
+
+		// Use SENSOR_TYPE_HEARTRATE_GEAR_LIVE below to have a better accuracy (only for Samsung Gear Live)
+		mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+		selHB = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent sensorEvent) {
+				lastHB = sensorEvent.values[0];
+			}
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int i) {}
+		};
+		mSensorManager.registerListener(selHB, this.mHeartRateSensor, 1000000); // Forced to 1s
+
+		mAccelerationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		selAcc = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent sensorEvent) {
+				lastAcc = sensorEvent.values;
+			}
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int i) {}
+		};
+		mSensorManager.registerListener(selAcc, this.mAccelerationSensor, SAMPLING_INTERVAL * 1000);
+
+		// ENABLE IF REQUIRED
+/*		mGyroscopeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		selGyroscope = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent sensorEvent) { lastGyro = sensorEvent.values; }
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int i) {}
+		};
+		mSensorManager.registerListener(selGyroscope, this.mGyroscopeSensor, SAMPLING_INTERVAL * 1000);
+
+		mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		selOrientation = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent sensorEvent) { lastOrientation = sensorEvent.values; }
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int i) {}
+		};
+		mSensorManager.registerListener(selOrientation, this.mOrientationSensor, SAMPLING_INTERVAL * 1000);
+
+		mStepSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+		selStep = new SensorEventListener() {
+			@Override
+			public void onSensorChanged(SensorEvent sensorEvent) { lastStepCount = sensorEvent.values[0]; }
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int i) {}
+		};
+		mSensorManager.registerListener(selStep, this.mStepSensor, 1000000); // Forced to 1s
+*/
+		File logFile = new File(Environment.getExternalStorageDirectory() + File.separator + "logs" + File.separator + "hb_log-" + sdf.format(new Date()) + ".txt");
+		try {
+			logFile.getParentFile().mkdirs();
+			logFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		try {
+			Log.d (TAG, "Log saved to : " + logFile.getAbsolutePath());
+			bw = new BufferedWriter(new FileWriter(logFile));
+			bw.write(String.format(Locale.ENGLISH, LOG_HEADER));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+		wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "SensorSamplingServiceWakeLock");
+	}
+
+	@Override
+	public int onStartCommand (Intent intent, int flags, int startId)
+	{
+		Log.i(TAG, "onStartCommand()");
+
+		isStarted = true;
+		wakeLock.acquire();
+
+		samples = 0;
+		isRunning = true;
+		t.start();
+
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy ()
+	{
+		Log.i(TAG, "onDestroy()");
+
+		isRunning = false;
+		t.interrupt();
+
+		if (wakeLock != null)
+			if (wakeLock.isHeld())
+				wakeLock.release();
+
+		if (bw != null) {
+			try {
+				bw.close();
+				Log.d (TAG, "File closed");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (selHB != null)
+			mSensorManager.unregisterListener(selHB);
+		if (selAcc != null)
+			mSensorManager.unregisterListener(selAcc);
+/*		if (selGyroscope != null)
+			mSensorManager.unregisterListener(selGyroscope);
+		if (selOrientation != null)
+			mSensorManager.unregisterListener(selOrientation);
+		if (selStep != null)
+			mSensorManager.unregisterListener(selStep);
+*/
+		isStarted = false;
+
+		super.onDestroy();
+	}
+}
